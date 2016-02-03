73ab2bca99d8fd91e32a7efc8943b1b1d3bc9734
oanabejenaru <bejenaru.oana.6@gmail.com>
2016-02-03 21:45:30 +0200

    Added facebook login option

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID' : '220554688287555', //App ID
+		'clientSecret' : '6687c7d571e413080eae47e7970405c7', //App Secret
+		'callbackURL' : 'http://localhost:8080/auth/facebook/callback'
+	}
+}
