896ce324d648240dc98fb1a7b577b791e5101bc5
oanabejenaru <bejenaru.oana.6@gmail.com>
2016-02-03 20:17:00 +0200

    Fixed login problem

--- app/routes.js
+++ app/routes.js
@@ -19,7 +19,11 @@ module.exports = function(app, passport) {
     });
 
     // process the login form
-    // app.post('/login', do all our passport stuff here);
+     app.post('/login',passport.authenticate('local-login',{
+        successRedirect : '/profile',
+        failureRedirect : '/login',
+        failureFlash : true 
+     }));
 
     // =====================================
     // SIGNUP ==============================
