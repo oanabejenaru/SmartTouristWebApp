b7e1646ab42959a7f1b76a7af6eee7434ccc9c28
oanabejenaru <bejenaru.oana.6@gmail.com>
2016-02-03 19:07:20 +0200

    Innitial project upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://oana:oana@apollo.modulusmongo.net:27017/rUpaso8g' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
